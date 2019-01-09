117e8fc06c3c61af8a6a0e8c3a1886065768893f
DaeunKang <jeangrey@xhiftcorp.com>
2019-01-09 18:14:14 +0900

    LogInScreen part One

--- components/AppContainer/presenter.js
+++ components/AppContainer/presenter.js
@@ -23,8 +23,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    //alignItems: "center",
+    //justifyContent: "center"
   }
 });
 
