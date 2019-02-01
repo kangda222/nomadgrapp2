1c66dddcf3402a5956f9d27d73b5b4550d339941
DaeunKang <jeangrey@xhiftcorp.com>
2019-02-01 10:10:21 +0900

    Making the FeedScreen part One

--- screens/FeedScreen/presenter.js
+++ screens/FeedScreen/presenter.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, RefreshControl, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
 
 const FeedScreen = props => (
-  <Text onPress={() => props.navigation.navigate("Likes")}>Feed</Text>
+    <ScrollView
+        refreshControl={
+        <RefreshControl
+            refreshing={props.isFetching}
+            onRefresh={props.refresh}
+            tintColor={"black"}
+        />
+        }
+        contentContainerStyle={styles.container}
+    />
 );
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "white"
+    }
+});
+  
+FeedScreen.propTypes = {
+    isFetching: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired
+};
 
 export default FeedScreen;
