19fb5ccceeb5d08e057c0fb3e81a82e659c01043
DaeunKang <jeangrey@xhiftcorp.com>
2019-02-08 11:12:34 +0900

    Creating the PhotoActions component

--- components/Photo/presenter.js
+++ components/Photo/presenter.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { View, Text, TouchableOpacity, Image, Dimensions, StyleSheet } from "react-native";
 import FadeIn from "react-native-fade-in-image";
+import PhotoActions from "../PhotoActions";
+import { withNavigation } from "react-navigation";
 
 const { width, height } = Dimensions.get("window");
 
@@ -34,6 +36,7 @@ const Photo = props => (
       />
     </FadeIn>
     <View style={styles.photoMeta}>
+      <PhotoActions isLiked={props.is_liked} likeCount={props.like_counts} />
       <View style={styles.comment}>
         <Text style={styles.commentAuthor}>
           {props.creator.username}{" "}
@@ -41,7 +44,7 @@ const Photo = props => (
         </Text>
       </View>
       {props.comments.length > 0 && (
-        <TouchableOpacity>
+        <TouchableOpacity onPressOut={() => props.navigation.navigate("Comments")}>
           <View style={styles.commentsLink}>          
             {props.comments.length === 1 ? (
               <Text style={styles.linkText}>View 1 comment</Text>
@@ -109,7 +112,7 @@ const styles = StyleSheet.create({
   dateText: {
     fontSize: 12,
     color: "#999",
-    marginTop: 5
+    marginTop: 10
   }
 });
 
@@ -138,4 +141,4 @@ Photo.propTypes = {
     is_vertical: PropTypes.bool.isRequired
 };
 
-export default Photo;
+export default withNavigation(Photo);
