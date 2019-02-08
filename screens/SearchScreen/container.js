72164a8936e6ab685e4d0bd187fff4bf28276cbc
DaeunKang <jeangrey@xhiftcorp.com>
2019-02-08 17:22:47 +0900

    SearchScreen part Two

--- screens/SearchScreen/container.js
+++ screens/SearchScreen/container.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import SearchScreen from "./presenter";
 import SearchBar from "../../components/SearchBar";
 
 class Container extends Component {
+  static propTypes = {
+    getEmptyFeed: PropTypes.func.isRequired,
+    searchHashtag: PropTypes.func.isRequired,
+    search: PropTypes.array
+  };
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state;
     return {
@@ -10,7 +16,8 @@ class Container extends Component {
     };
   };
   state = {
-    searchingBy: ""
+    searchingBy: "",
+    isFetching: false
   };
   componentDidMount() {
     const { navigation } = this.props;
@@ -18,15 +25,36 @@ class Container extends Component {
       submitSearch: this._submitSearch
     });
   }
+  componentWillReceiveProps = nextProps => {
+    if (nextProps.search) {
+      this.setState({
+        isFetching: false
+      });
+    }
+  };
   render() {
-    return <SearchScreen {...this.state} />;
+    return (
+        <SearchScreen {...this.state} {...this.props} refresh={this._refresh} />
+    );
   }
   _submitSearch = text => {
     const { searchingBy } = this.state;
+    const { searchHashtag } = this.props;
     this.setState({
-      searchingBy: text
+      searchingBy: text,
+      isFetching: true
     });
     // call api and search by hashtag
+    searchHashtag(text);    
+  };
+  _refresh = () => {
+    const { searchingBy } = this.state;
+    const { getEmptyFeed, searchHashtag } = this.props;
+    if (searchingBy === "") {
+      getEmptyFeed();
+    } else {
+      searchHashtag(searchingBy);
+    }
   };
 }
 
