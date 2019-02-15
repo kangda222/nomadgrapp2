efc4db84c654874d3647f36ec889e2493c00ca05
DaeunKang <jeangrey@xhiftcorp.com>
2019-02-15 17:24:42 +0900

    Uploading a Photo part Four (Taking a Photo)

--- screens/CameraScreen/index.js
+++ screens/CameraScreen/index.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Camera, Permissions } from "expo";
+import FitImage from "react-native-fit-image";
 
 class CameraScreen extends Component {
     state = {
@@ -27,7 +28,13 @@ class CameraScreen extends Component {
     };
   
     render() {
-      const { hasCameraPermissions, type, flash } = this.state;
+      const {
+            hasCameraPermissions,
+            type,
+            flash,
+            pictureTaken,
+            picture
+      } = this.state;
       if (hasCameraPermissions === null) {
         return <View />;
       } else if (hasCameraPermissions === false) {
@@ -35,6 +42,11 @@ class CameraScreen extends Component {
       } else {
         return (
           <View style={styles.container}>
+            {pictureTaken ? (
+            <View style={{ flex: 2 }}>
+              <FitImage source={{ uri: picture }} style={{ flex: 1 }} />
+            </View>
+            ) : (
             <Camera
               type={type}
               flashMode={flash}
@@ -56,20 +68,25 @@ class CameraScreen extends Component {
               </TouchableOpacity>
               <TouchableOpacity onPressOut={this._changeFlash}>
                 <View style={styles.action}>
-                    {flash === Camera.Constants.FlashMode.off && (
+                  {flash === Camera.Constants.FlashMode.off && (
                     <MaterialIcons name={"flash-off"} color="white" size={40} />
-                    )}
-                    {flash === Camera.Constants.FlashMode.on && (
+                  )}
+                  {flash === Camera.Constants.FlashMode.on && (
                     <MaterialIcons name={"flash-on"} color="white" size={40} />
-                    )}
-                    {flash === Camera.Constants.FlashMode.auto && (
-                    <MaterialIcons name={"flash-auto"} color="white" size={40} />
-                    )}
+                  )}
+                  {flash === Camera.Constants.FlashMode.auto && (
+                    <MaterialIcons
+                      name={"flash-auto"}
+                      color="white"
+                      size={40}
+                    />
+                  )}
                 </View>
               </TouchableOpacity>
             </Camera>
+            )}
             <View style={styles.btnContainer}>
-              <TouchableOpacity>
+              <TouchableOpacity onPressOut={this._takePhoto}>
                 <View style={styles.btn} />
               </TouchableOpacity>
             </View>
@@ -77,29 +94,42 @@ class CameraScreen extends Component {
         );
       }
     }
+
+    _changeType = () => {
+        this.setState(prevState => {
+          if (prevState.type === Camera.Constants.Type.back) {
+            return { type: Camera.Constants.Type.front };
+          } else {
+            return { type: Camera.Constants.Type.back };
+          }
+        });
+    };
+    
+    _changeFlash = () => {
+        this.setState(prevState => {
+          if (prevState.flash === Camera.Constants.FlashMode.off) {
+            return { flash: Camera.Constants.FlashMode.on };
+          } else if (prevState.flash === Camera.Constants.FlashMode.on) {
+            return { flash: Camera.Constants.FlashMode.auto };
+          } else if (prevState.flash === Camera.Constants.FlashMode.auto) {
+            return { flash: Camera.Constants.FlashMode.off };
+          }
+        });
+    };
+    
+    _takePhoto = async () => {
+        const { pictureTaken } = this.state;
+        if (!pictureTaken) {
+          if (this.camera) {
+            const takenPhoto = await this.camera.takePictureAsync({
+              quality: 0.5,
+              exif: true
+            });
+            this.setState({ picture: takenPhoto.uri, pictureTaken: true });
+          }
+        }
+    };
 }
-
-_changeType = () => {
-    this.setState(prevState => {
-      if (prevState.type === Camera.Constants.Type.back) {
-        return { type: Camera.Constants.Type.front };
-      } else {
-        return { type: Camera.Constants.Type.back };
-      }
-    });
-};
-
-_changeFlash = () => {
-    this.setState(prevState => {
-      if (prevState.flash === Camera.Constants.FlashMode.off) {
-        return { flash: Camera.Constants.FlashMode.on };
-      } else if (prevState.flash === Camera.Constants.FlashMode.on) {
-        return { flash: Camera.Constants.FlashMode.auto };
-      } else if (prevState.flash === Camera.Constants.FlashMode.auto) {
-        return { flash: Camera.Constants.FlashMode.off };
-      }
-    });
-};
   
 const styles = StyleSheet.create({
     container: {
